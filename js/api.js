f9278784ed76806c897f93fbbc2f575c343d966b
JoeXin <1203970535@qq.com>
2025-06-23 13:52:18 +0800

    api请求

--- js/api.js
+++ js/api.js
@@ -550,7 +550,7 @@ async function handleMultipleCustomSearch(searchQuery, customApiUrls) {
     window.fetch = async function(input, init) {
         const requestUrl = typeof input === 'string' ? new URL(input, window.location.origin) : input.url;
         
-        if (requestUrl.pathname.startsWith('/api/')) {
+        if (requestUrl.pathname.startsWith('/LibreTV/api/')) {
             if (window.isPasswordProtected && window.isPasswordVerified) {
                 if (window.isPasswordProtected() && !window.isPasswordVerified()) {
                     return;
