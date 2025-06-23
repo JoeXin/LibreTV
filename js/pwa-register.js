685f070e95b5ebe9da482919985fe52f526a7ae3
JoeXin <1203970535@qq.com>
2025-06-23 14:33:16 +0800

    11

--- js/pwa-register.js
+++ js/pwa-register.js
@@ -1,7 +1,7 @@
 // PWA 注册
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        // navigator.serviceWorker.register('/LibreTV/service-worker.js');
+         navigator.serviceWorker.register('/LibreTV/service-worker.js');
         // navigator.serviceWorker.register('/service-worker.js');
 
     });
